73d4873546ca4ed1b668b25f8bd84e760dc3dade
GamerBoss101 <adithcool.5@gmail.com>
2024-09-14 22:47:36 -0400

    Lots of Changes

---
+++ src/lib/components/form/QuestionSelect.svelte
@@ -0,0 +1,72 @@
+<script>
+    // @ts-nocheck
+    import Survey from "./Survey.svelte";
+    export let parentDiv;
+
+    console.log(parentDiv);
+
+    function open5Survey(questions) {
+        console.log(questions);
+
+        parentDiv.innerHTML = "";
+        new Survey({
+            target: parentDiv,
+            props: {
+                numQuestion: 5
+            }
+        });
+    }
+
+    function open10Survey(questions) {
+        console.log(questions);
+
+        parentDiv.innerHTML = "";
+        new Survey({
+            target: parentDiv,
+            props: {
+                numQuestion: 10
+            }
+        });
+    }
+
+    function open15Survey(questions) {
+        console.log(questions);
+
+        parentDiv.innerHTML = "";
+        new Survey({
+            target: parentDiv,
+            props: {
+                numQuestion: 15
+            }
+        });
+    }
+    
+
+</script>
+
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body mx-auto">
+                <button class="btn btn-primary" href="#form" on:click={open5Survey} type="button" style="padding: 30px;border-radius: 15px;font-size: 24px;">5 Questions</button>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body mx-auto">
+                <button class="btn btn-primary" href="#form" on:click={open10Survey} type="button" style="padding: 30px;border-radius: 15px;font-size: 24px;">10 Questions</button>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body mx-auto">
+                <button class="btn btn-primary" href="#form" on:click={open15Survey} type="button" style="padding: 30px;border-radius: 15px;font-size: 24px;">15 Questions</button>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<p class="card-text" style="--bs-body-font-size: 1rem;font-size: 24px;"><strong><span style="background-color: transparent;">The more questions you answer, the more personalized your recommendation will be!</span></strong></p>
---
+++ src/routes/survey/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    // @ts-nocheck
+    import { onMount } from "svelte";
+    import QuestionSelect from "$lib/components/form/QuestionSelect.svelte";
+
+    let parentDiv;
+
+    onMount(() => {
+        let r = new QuestionSelect({
+            target: parentDiv,
+            props: {
+                parentDiv
+            }
+        });
+    });
+
+</script>
+
+<section class="py-5">
+    <div class="container py-5">
+        <div class="row mb-5" style="padding-bottom: 20px;">
+            <div class="col-md-8 col-xl-6 text-center mx-auto">
+                <h2 class="fw-bold"><strong><span style="background-color: transparent;">How many questions do you want to answer?</span></strong></h2>
+            </div>
+        </div>
+        <div id="form" class="card" style="padding-top: 20px;padding-bottom: 100px;">
+            <div bind:this={parentDiv} class="card-body mx-auto"></div>
+        </div>
+    </div>
+</section>
