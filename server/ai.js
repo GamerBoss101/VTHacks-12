53cb0b13126472d94bf3fbe3317146dd108879ca
Auth <93170896+AuthGithub@users.noreply.github.com>
2024-09-14 20:40:18 -0400

    aiqaupdated

--- server/ai.js
+++ server/ai.js
@@ -29,12 +29,11 @@ const response = await ollama.chat({
 const questions = [
     "Is this for breakfast, lunch, dinner, snack or dessert?",
     "What kind of cuisine are you in the mood for? (e.g., Italian, Mexican, Asian)",
-    "Do you want a light or heavy meal?",
     "Want something healthy or more of a treat?",
     "Are there any specific ingredients you love?",
     "Any specific ingredients that you dislike?",
     "Are you looking for something quick and easy, or are you willing to spend more time cooking?",
-    "What’s your go-to protein source? (e.g., chicken, tofu, fish)",
+    "What’s your go-to protein source? (e.g., chicken, tofu, fish, no protein)",
     "How spicy do you want to have the food?",
     "What drink do you want to pair up with your meal?",
     "Is this meal for a special occasion, or just a regular day?",
@@ -42,9 +41,27 @@ const questions = [
     "What tools do you have at your culinary disposal? (Stovetop, oven, microwave, blender?)",
     "Do you prefer recipes that yield leftovers (great for meal prep) or single-serving portions?",
     "Are you looking for budget-friendly options, or are you willing to splurge a bit for a special meal?",
-    "What’s a calorie range you want your meal to stay in?"
+    "What’s the calorie range you want your meal to stay in? (You can specify yes or no)"
 ]
 const testAnswers =[
+    "Lunch",
+    "Italian",
+    "Healthy",
+    "Cheese",
+    "Beans",
+    "Moderate cook time",
+    "Chicken",
+    "Medium",
+    "Wine",
+    "Regular Day",
+    "Crispy",
+    "Oven,Microwave,Stove",
+    "few leftovers",
+    "Budget-Friendly",
+    "800-1000"
+]
+
+const restrictions=[
 
 ]
 
